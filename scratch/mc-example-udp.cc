b852f6fe7fd1ab79a69b41c95e333d9a6f8a56aa
mychele <michele.polese@gmail.com>
2016-10-24 18:49:54 +0200

    Change max val of fixed TTT

--- scratch/mc-example-udp.cc
+++ scratch/mc-example-udp.cc
@@ -743,6 +743,8 @@ main (int argc, char *argv[])
         Config::SetDefault ("ns3::LteEnbRrc::SecondaryCellHandoverMode", EnumValue(LteEnbRrc::DYNAMIC_TTT));
         break;
   }
+  
+  Config::SetDefault ("ns3::LteEnbRrc::FixedTttValue", UintegerValue (150));
   Config::SetDefault ("ns3::LteEnbRrc::CrtPeriod", IntegerValue (ReportTablePeriodicity));
   Config::SetDefault ("ns3::LteEnbRrc::OutageThreshold", DoubleValue (outageTh));
   Config::SetDefault ("ns3::MmWaveEnbPhy::UpdateSinrEstimatePeriod", IntegerValue (ReportTablePeriodicity));
