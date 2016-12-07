c74612bd24b628ed41009463e2fa3c5e0c0fb754
zml312570140 <mengleizhang.mz@gmail.com>
2016-12-07 14:54:22 -0500

    Add copyright to model files END

--- src/mmwave/model/mmwave-beamforming.h
+++ src/mmwave/model/mmwave-beamforming.h
@@ -1,9 +1,27 @@
-/*
- * mmWave-beamforming.h
- *
- *  Created on: 2014年11月25日
- *      Author: menglei
+ /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+ /*
+ *   Copyright (c) 2015, NYU WIRELESS, Tandon School of Engineering, New York University
+ *  
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2 as
+ *   published by the Free Software Foundation;
+ *  
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *  
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  
+ *  
+ *   Author: Marco Mezzavilla < mezzavilla@nyu.edu>
+ *        	 Sourjya Dutta <sdutta@nyu.edu>
+ *        	 Russell Ford <russell.ford@nyu.edu>
+ *        	 Menglei Zhang <menglei@nyu.edu>
  */
+
 
 #ifndef MMWAVE_BEAMFORMING_H_
 #define MMWAVE_BEAMFORMING_H_
