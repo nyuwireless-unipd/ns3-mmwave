88d167912f22ea94bdc7b284c573d14b84e699e1
zml312570140 <mengleizhang.mz@gmail.com>
2016-12-07 14:54:22 -0500

    Add copyright to model files END

--- src/mmwave/model/mmwave-mac-csched-sap.h
+++ src/mmwave/model/mmwave-mac-csched-sap.h
@@ -1,22 +1,30 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-/*
- * Copyright (c) 2011 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+ /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+ /*
+ *   Copyright (c) 2011 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+ *   Copyright (c) 2015, NYU WIRELESS, Tandon School of Engineering, New York University
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2 as
+ *   published by the Free Software Foundation;
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Manuel Requena <manuel.requena@cttc.es>
+ *   Author: Marco Miozzo <marco.miozzo@cttc.es>
+ *           Nicola Baldo  <nbaldo@cttc.es>
+ *
+ *   Modified by: Marco Mezzavilla < mezzavilla@nyu.edu>
+ *        	 	  Sourjya Dutta <sdutta@nyu.edu>
+ *        	 	  Russell Ford <russell.ford@nyu.edu>
+ *        		  Menglei Zhang <menglei@nyu.edu>
  */
+
 
 #ifndef MMWAVE_MAC_CSCHED_SAP_H
 #define MMWAVE_MAC_CSCHED_SAP_H
