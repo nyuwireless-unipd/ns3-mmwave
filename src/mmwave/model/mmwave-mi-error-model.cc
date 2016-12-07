af8854db28615c4ab6ba2d7094d78a8219a8e347
zml312570140 <mengleizhang.mz@gmail.com>
2016-12-07 14:50:47 -0500

    Add Copyright to model files

--- src/mmwave/model/mmwave-mi-error-model.cc
+++ src/mmwave/model/mmwave-mi-error-model.cc
@@ -1,33 +1,30 @@
-/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
-/*
-* Copyright (c) 2011 SIGNET LAB. Department of Information Engineering (DEI), University of Padua
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2 as
-* published by the Free Software Foundation;
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*
-*
-* Original Work Authors:
-*      Marco Mezzavilla <mezzavil@dei.unipd.it>
-*      Giovanni Tomasi <tomasigv@gmail.com>
-* Original Work Acknowldegments:
-*      This work was supported by the MEDIEVAL (MultiMEDia transport
-*      for mobIlE Video AppLications) project, which is a
-*      medium-scale focused research project (STREP) of the 7th
-*      Framework Programme (FP7)
-*
-* Subsequent integration in LENA and extension done by:
-*      Marco Miozzo <marco.miozzo@cttc.es>
-*/ 
+ /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+ /*
+ *   Copyright (c) 2011 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+ *   Copyright (c) 2015, NYU WIRELESS, Tandon School of Engineering, New York University
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2 as
+ *   published by the Free Software Foundation;
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *   Author: Marco Miozzo <marco.miozzo@cttc.es>
+ *           Nicola Baldo  <nbaldo@cttc.es>
+ *
+ *   Modified by: Marco Mezzavilla < mezzavilla@nyu.edu>
+ *        	 	  Sourjya Dutta <sdutta@nyu.edu>
+ *        	 	  Russell Ford <russell.ford@nyu.edu>
+ *        		  Menglei Zhang <menglei@nyu.edu>
+ */
+
 
 
 #include <list>
